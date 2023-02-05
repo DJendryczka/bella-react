1ebf813d06c4ba1c7b764fac33d26d7a6f79fa0b
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-02-05 12:50:47 +0100

    Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { RiMenuUnfoldLine } from "react-icons/ri";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className=" absolute w-full flex justify-between p-4 items-center">
+      <h1 className=" text-white font-bold  text-2xl  z-20 ">BellaStäd</h1>
+      <RiMenuUnfoldLine
+        size={35}
+        className=" z-20 text-white  cursor-pointer"
+        onClick={handleNav}
+      />
+      <div
+        className={
+          nav
+            ? " fixed text-gray-300 left-0 top-0  w-full h-screen bg-black/80 px-4 py-7 flex-col z-10 ease-in duration-300"
+            : " absolute  top-0 h-screen left-[-100%] ease-in duration-500 z-10"
+        }
+      >
+        <ul className=" flex flex-col fixed w-full h-full items-center justify-center">
+          <li className=" font-bold text-3xl p-8 cursor-pointer">HEM</li>
+          <li className=" font-bold text-3xl p-8 cursor-pointer">OM OSS</li>
+          <li className=" font-bold text-3xl p-8 cursor-pointer">TJÄNSTER</li>
+          <li className=" font-bold text-3xl p-8 cursor-pointer">KONTAKT</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
